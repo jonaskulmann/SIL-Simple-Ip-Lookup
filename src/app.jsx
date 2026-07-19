1da80b75c60b80401f57fb7c2f37df815d642eea
alemaoazindev <jonaskazevedo@gmail.com>
2026-07-19 17:38:15 -0300

    ACHO QUE FINALMENTE CONSEGUI COMMITAR A PASTA DE SOURCE

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>teste estrutura</h1>
+      <p>mds tomara q funcione</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
